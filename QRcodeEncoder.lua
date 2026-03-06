dee5f9f0b24032ee7b5ea7412691d0250776480f
Station <station384@gmail.com>
2026-03-06 15:55:52 -0500

    Make sure the license info is clear.

--- QRcodeEncoder.lua
+++ QRcodeEncoder.lua
@@ -1,3 +1,33 @@
+--- The qrcode library is licensed under the 3-clause BSD license (aka "new BSD")
+--- To get in contact with the author, mail to <gundlach@speedata.de>.
+---
+--- Please report bugs on the [github project page](http://speedata.github.io/luaqrcode/).
+-- Copyright (c) 2012-2020, Patrick Gundlach and contributors, see https://github.com/speedata/luaqrcode
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--	 * Redistributions of source code must retain the above copyright
+--	   notice, this list of conditions and the following disclaimer.
+--	 * Redistributions in binary form must reproduce the above copyright
+--	   notice, this list of conditions and the following disclaimer in the
+--	   documentation and/or other materials provided with the distribution.
+--	 * Neither the name of SPEEDATA nor the
+--	   names of its contributors may be used to endorse or promote products
+--	   derived from this software without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL SPEEDATA GMBH BE LIABLE FOR ANY
+-- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+-- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+-- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+-- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 local debugging = false;
 local testing = false;
 
@@ -31,38 +61,6 @@ local testing = false;
 --   local ret = string.format(fmtstring,s)
 --   return string.gsub(ret," ","0")
 -- end
-
-
-
-
---- The qrcode library is licensed under the 3-clause BSD license (aka "new BSD")
---- To get in contact with the author, mail to <gundlach@speedata.de>.
----
---- Please report bugs on the [github project page](http://speedata.github.io/luaqrcode/).
--- Copyright (c) 2012-2020, Patrick Gundlach and contributors, see https://github.com/speedata/luaqrcode
--- All rights reserved.
---
--- Redistribution and use in source and binary forms, with or without
--- modification, are permitted provided that the following conditions are met:
---	 * Redistributions of source code must retain the above copyright
---	   notice, this list of conditions and the following disclaimer.
---	 * Redistributions in binary form must reproduce the above copyright
---	   notice, this list of conditions and the following disclaimer in the
---	   documentation and/or other materials provided with the distribution.
---	 * Neither the name of SPEEDATA nor the
---	   names of its contributors may be used to endorse or promote products
---	   derived from this software without specific prior written permission.
---
--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
--- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
--- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
--- DISCLAIMED. IN NO EVENT SHALL SPEEDATA GMBH BE LIABLE FOR ANY
--- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
--- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
--- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
--- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
--- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
--- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 --- Overall workflow
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RuneReaderAddonVoice
 Addon for getting dialog info out of WOW
+
+## License
+
+This project is licensed under the GNU General Public License, version 3 or later (GPL-3.0-or-later).
+
+See the `COPYING` file for the full license text.
