da74b6e96feb33bdf120afd14fe35ad8b9890b4e
Station <station384@gmail.com>
2026-04-02 13:47:46 -0400

    Update License info to correct one.

--- base45.lua
+++ base45.lua
@@ -1,3 +1,21 @@
+-- SPDX-License-Identifier: GPL-3.0-or-later
+--
+-- This file is part of RuneReaderVoice.
+--
+-- Copyright (C) 2026 Michael Sutton
+--
+-- RuneReaderVoice is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- RuneReaderVoice is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with RuneReaderVoice. If not, see <https://www.gnu.org/licenses/>.
 
 -- QR-safe Base45 for Lua 5.1
 -- Alphabet is exactly the QR alphanumeric-safe set (45 chars)
